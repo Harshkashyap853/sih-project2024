ec9fa68b663524e2c22d634d5b2452da324b1d7d
HarshKashyap <harshkashyap853@gmail.com>
2024-09-05 09:03:51 +0530

    firstversion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css';
+import Homepage from "./Components/Homepage";
+import Bodybuilding from './Components/Bodybuiding/Bodybuilding';
+import Education from './Components/Education';
+import BlogDetail from './Components/education/[id]/Blog';
+import BMICALCULATE from './Components/Calculator'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/bodybuilding" element={<Bodybuilding/>}/>
+          <Route path='/Education' element={<Education/>}/>
+          <Route path="/education/:id" element={<BlogDetail/>} />
+          <Route path="/calculator" element={<BMICALCULATE/>}/>
+     </Routes>
+      </BrowserRouter>
+    </>
+    
   );
 }
 
